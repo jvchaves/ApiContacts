e3bab087e08eeddc605fbb667b6752d1de329e7c
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-08-31 09:12:28 -0300

    Adiciono o autor do projeto

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -1,3 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+  def author
+    "João Victor Chaves"
+  end
+
+  def as_json(options = nil)
+    super(methods: :author, root: true)
+  end
 end
