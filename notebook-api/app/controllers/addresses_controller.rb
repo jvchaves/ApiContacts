9e92a9be1410f3ae65056b72cfbd6ff1b3fe97dc
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-09-13 15:31:30 -0300

    Crio def para criar o address do contact

--- notebook-api/app/controllers/addresses_controller.rb
+++ notebook-api/app/controllers/addresses_controller.rb
@@ -1,5 +1,14 @@
 class AddressesController < ApplicationController
   before_action :set_contact
+
+  def create
+    @contact.address.new(address_params)
+    if @contact.save
+      render json: @contact.address, status: :created, location: contact_address_url(@contact)
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
   def update
     if @contact.address.update(address_params)
       render json: @contact.address
